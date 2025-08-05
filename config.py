3d92d49e327bfe519136f320414e8f032f558678
sushantm04 <sushantm04@gmail.com>
2025-08-05 10:28:02 +0530

    Adding 3 files to repo

---
+++ config.py
@@ -0,0 +1,6 @@
+file_path = r"pan1.png"
+pytesseract_path = r"C:\Users\Admin\AppData\Local\Tesseract-OCR\tesseract.exe"
+#hello i am inside configuration file
+# added new line successfully
+
+# one more new line added successfully
